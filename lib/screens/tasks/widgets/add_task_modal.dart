287c628e6208079f3c2878326291cbf133607011
Amin <amin.arshadinia@whipmobility.com>
2023-06-14 10:49:15 +0800

    feat: create modal content

---
+++ lib/screens/tasks/widgets/add_task_modal.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:task_management_bloc/models/task.dart';
+
+import '../../../application/blocs_export.dart';
+
+class AddTaskModal extends StatelessWidget {
+  const AddTaskModal({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController titleController = TextEditingController();
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          const Text(
+            'Add task',
+            style: TextStyle(fontSize: 24),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          TextField(
+            autofocus: true,
+            controller: titleController,
+            decoration: const InputDecoration(
+              label: Text('Title'),
+              border: OutlineInputBorder(),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: const Text('cancel'),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  var task = Task(title: titleController.text);
+                  context.read<TasksBloc>().add(AddTask(task: task));
+                  Navigator.pop(context);
+                },
+                child: const Text('Add'),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
